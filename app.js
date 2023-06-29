6487fccc2259477b237895e565017295a04fc94e
Away <agwamayatullah@gmail.com>
2023-06-29 19:43:46 +0700

    Create RESTful API

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const ejs = require('ejs')
 
 const app = express();
 
+app.set('view-engine', 'ejs')
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
 
-mongoose.connect('mongodb://127.0.0.1:27071/wikiDB');
+mongoose.connect('mongodb://0.0.0.0:27017/wikiDB');
 
-const Article = new mongoose.Schema({
+const ArticleSchema = new mongoose.Schema({
   title: String,
   content: String,
   createAt: {
@@ -17,4 +21,51 @@ const Article = new mongoose.Schema({
   }
 });
 
-const articleModel = new mongoose.model('articles', Article);
+const Article = new mongoose.model('articles', ArticleSchema);
+
+/* ----- All Articles ----- */
+
+app.route('/articles')
+  .get((req, res) => {
+    Article.find({}).then(data => res.send(data))
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+  
+    newArticle.save().then(err => res.send(err))
+  })
+  .delete((req, res) => {
+    Article.deleteMany({}).then(err => res.send(err))
+  });
+
+app.listen(3000, () => {
+  console.log('runnning on 3000')
+})
+
+/* ----- Specified Articles ----- */
+
+app.route('/articles/:title')
+  .get((req, res) => {
+    Article.findOne({title: req.params.title}).then(data => {
+      if (!data){
+        res.send('No data')
+      } else{
+        res.send(data)
+      }
+    })
+  })
+  .put((req, res) => {
+    Article.replaceOne({title: req.params.title}, req.body)
+    .then(data => res.send(data))
+  })
+  .patch((req, res) => {
+    Article.updateOne({title: req.params.title}, req.body)
+    .then(data => res.send(data))
+  })
+  .delete((req, res) => {
+    Article.deleteOne({title: req.params.title})
+    .then(data => res.send(data))
+  });
